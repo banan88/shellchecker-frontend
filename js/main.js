e55774b43e1afa96fb04e88d9d985ae23fba5f48
banan@work <mat.banaszkiewicz@gmail.com>
2015-09-25 16:37:51 +0200

    file upload (pure js) button added

--- js/main.js
+++ js/main.js
@@ -1,11 +1,24 @@
-
-$(document).ready(function() {
-
-    $("#check").click(function () {
-        source_script={source: $("#source").val()};
-        $.post("/", source_script,function (data, status) {
+var check_pasted_script = function () {
+        source_script = {source: $("#source").val()};
+        $.post("/", source_script, function (data, status) {
 
             $("#target").html(data);
         });
-    });
+    };
+
+var handleFiles = function (files) {
+    var file = files[0];
+    if (file) {
+        var reader = new FileReader();
+        reader.readAsText(file, "UTF-8");
+        reader.onload = function (evt) {
+            $("#source").html(evt.target.result);
+            check_pasted_script();
+        }
+    }
+};
+
+$(document).ready(function () {
+    $("#check").click(check_pasted_script);
 });
+
